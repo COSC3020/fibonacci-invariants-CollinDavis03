2d72cedf5d4c6dc142dcc596c940ef400bd565e4
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-09-17 11:08:35 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
+function Fiboancci(n, list = []){
+    if(n < 1) {
+        return n; 
+    } 
+
+    if (list[n] !== undefined) {
+        return list[n]; 
+    }
+
+    list[n] = fib(n -1, list) + fib(n - 2, list);
+    return list[n]; 
+}
+
 function fib(n) {
-    return n;
+    let Fibo = []; 
+    for (let i = 0; i <= n; i++){
+        Fibo[i] = fib(i); 
+    }
+    return Fibo;
 }
