d01647b221fa87dfe616f634ea06b28476397054
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-09-24 09:44:53 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,24 +1,25 @@
-function fib(n, list = []){
-    if(n == 0) {
-        return 0; 
-    } 
-
-    if (n == 1) {
-        return 1; 
-    }
-
-    if (list[n] != undefined) {
-        return list[n]; 
-    }
-
-    list[n] = fib(n - 1, list) + fib(n - 2, list);
-    return list[n]; 
-}
-
 function Fibonacci(n) {
     let Fibo = []; 
     for (let i = 0; i <= n; i++){
         Fibo[i] = fib(i, Fibo); 
     }
     return Fibo;
+}
+
+
+function fib(n, list = []){
+    if(n === 0) {
+        return 0; 
+    } 
+
+    if (n === 1) {
+        return 1; 
+    }
+
+    if (list[n] !== undefined) {
+        return list[n]; 
+    }
+
+    list[n] = fib(n - 1, list) + fib(n - 2, list);
+    return list[n]; 
 }
