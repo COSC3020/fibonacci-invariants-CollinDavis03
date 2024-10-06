4a3bfd74bb2010aa58996889d2296691f75f40fa
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-10-06 13:46:53 -0600

    Update code.js

--- code.js
+++ code.js
@@ -8,12 +8,11 @@ if (n == 0) {
 
 if (n == 1) {
     return arr[1]; 
-} else { 
-    for (let i = 2; i <= n; i++) 
-    { 
-        arr.push(arr[i-1]+arr[i-2]);
+} 
+else { 
+    for (let i = 2; i <= n; i++) { 
+        arr.push(arr[i-1]+arr[i-2])
     }
 }
-
 return arr; 
 }
