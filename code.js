954d848fc165aee566294fd4982d4692c3f44ecb
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-10-09 10:31:29 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
 function fib(a) { 
-if (a === 0) {
-    return 0;
-}
 
-if (a === 1) {
-    return 1; 
-} 
-return fib(a-1) + fib(a - 2); 
+    if (a === 0) return [0];
+    if (a === 1) return [0, 1];
+        
+    const arr = fib(a - 1);
+
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    
+return arr; 
 }
