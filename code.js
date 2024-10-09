ec4c017a463a3e98e001b4325e7db7052578b2ef
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-10-09 10:24:37 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,10 @@
 function fib(a) { 
-
-let arr = [0,1]
-
-if (a == 0) {
-    return [0];
+if (a === 0) {
+    return 0;
 }
 
-if (a == 1) {
-    return [0,1]; 
+if (a === 1) {
+    return 1; 
 } 
-else { 
-    for (let i = 2; i <= a; i++) { 
-        arr.push(arr[i-1] + arr[i-2])
-    }
-}
-return arr; 
+return fib(a-1) + fib(a - 2); 
 }
