aa687c53fc99ff1870e976d95ba2c934e868ac09
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-10-29 08:27:36 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,8 @@ function fib(a) {
     if (a === 0) return [0];
     if (a === 1) return [0, 1];
         
-    const arr = fib(a - 1);
+    const arr = fib(a - 1); //thought this was Invariant becasue it applies to the equation below and only allows the equation to happen if it has met up to that number in
+    //sequence. 
 
     arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
     
